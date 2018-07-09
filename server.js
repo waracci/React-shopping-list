e3e045746c00dde730706c6590e124ac34961f1a
waracci <morriswarachi0@gmail.com>
2018-07-09 11:10:19 +0300

    alter port to PORT

--- server.js
+++ server.js
@@ -33,7 +33,7 @@ if (process.env.NODE_ENV === 'production') {
     });
 }
 
-const port = process.env.port || 5000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`server started on port ${port}`)
 });
